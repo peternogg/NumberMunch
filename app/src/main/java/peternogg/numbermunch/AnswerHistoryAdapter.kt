b11819b431130d3a80ac2a4afdd929143cf368e7
peternogg <peternogg@gmail.com>
2018-05-08 19:38:49 -0700

    Rebuilt AnswerHistoryAdapter to use data binding
    Also formatted answer_entry to look nice(r) and be centered on the screen.
    
    Changed the string for the user answer entry box hint because it was too long

--- app/src/main/java/peternogg/numbermunch/AnswerHistoryAdapter.kt
+++ app/src/main/java/peternogg/numbermunch/AnswerHistoryAdapter.kt
@@ -1,29 +1,31 @@
 package peternogg.numbermunch
 
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.answer_entry.view.*
+import peternogg.numbermunch.databinding.AnswerEntryBinding
 
 class AnswerHistoryAdapter(val history: MutableList<SolvedProblem>) : RecyclerView.Adapter<AnswerHistoryAdapter.ViewHolder>() {
 
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder(val binding: AnswerEntryBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(problem: SolvedProblem) {
+            binding.problem = problem
+            binding.executePendingBindings()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val row = LayoutInflater.from(parent.context).inflate(R.layout.answer_entry, parent, false)
-
-        return ViewHolder(row)
+        val row = LayoutInflater.from(parent.context)
+        val binding = AnswerEntryBinding.inflate(row, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val problem = history[position]
-
-        holder.view.Question.text = problem.questionString()
-        holder.view.UserAnswer.text = problem.answer.toString()
-        holder.view.CorrectAnswer.text = problem.result.toString()
-        holder.view.CorrectAnswer.visibility = if (problem.isCorrect) View.INVISIBLE else View.VISIBLE
+        holder.bind(history[position])
     }
 
     override fun getItemCount(): Int {
