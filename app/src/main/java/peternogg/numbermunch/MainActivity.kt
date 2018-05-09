360c0f261c066d800c16343755e2f3d4064acf25
peternogg <peternogg@gmail.com>
2018-05-08 19:01:34 -0700

    Fixed the RecyclerView and removed unnecessary members in MainActivity
    Turns out, you have to set up binding before you set up the RecyclerView. They interact in a weird way, I think because they both act in a similar way.

--- app/src/main/java/peternogg/numbermunch/MainActivity.kt
+++ app/src/main/java/peternogg/numbermunch/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import org.jetbrains.anko.toast
@@ -12,29 +13,24 @@ import peternogg.numbermunch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var historyAdapter: AnswerHistoryAdapter
-    private lateinit var model: ProblemModel
+    private val historyAdapter = AnswerHistoryAdapter(mutableListOf())
+    private val model = ProblemModel(historyAdapter)
+    private lateinit var historyDisplay: RecyclerView
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        historyAdapter = AnswerHistoryAdapter(mutableListOf(SolvedProblem(Problem(1, 2, '+'), 3)))
-        viewManager = LinearLayoutManager(this)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.model = model
 
-        recyclerView = findViewById<RecyclerView>(R.id.EntriesList).apply {
+        val viewManager = LinearLayoutManager(this)
+        historyDisplay = findViewById<RecyclerView>(R.id.EntriesList).apply {
             setHasFixedSize(true)
             adapter = historyAdapter
             layoutManager = viewManager
         }
-
-        model = ProblemModel(historyAdapter)
-
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.model = model
 
         findViewById<EditText>(R.id.AnswerEntry).setOnEditorActionListener(({_, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEND) {
